5cfa60aee6d2e80c4adf29e8d09c199c9f6f6485
sager <sager@hpb.io>
2022-06-02 15:30:27 +0800

    update example.

--- contracts/example/Consumer.sol
+++ contracts/example/Consumer.sol
@@ -7,10 +7,18 @@ import "../common/Auth.sol";
 contract ComsumerExample is IConsumerBase, Admin {
     uint256 _nrandom;
 
+    address [] players;
+
     IOracle oracle = IOracle(0xd9145CCE52D386f254917e481eB44e9943F39138); // oracle address
+
+    event GameWinner(address winner, uint256 block);
 
     function startNewGame() public onlyOwner {
         oracle.requestRandom(address(this));
+    }
+
+    function joinGame() public {
+        players.push(msg.sender);
     }
 
     // responseRandom will 
@@ -19,4 +27,10 @@ contract ComsumerExample is IConsumerBase, Admin {
         return true;
     }
 
+    function endGame() public onlyOwner {
+        require(_nrandom != 0, "not got random");
+        require(players.length > 0, "have no players");
+        uint32 wineridx = uint32(_nrandom% players.length);
+        emit GameWinner(players[wineridx], block.number);
+    }
 }
